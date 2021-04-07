b46279c2ada0c15daaaa3d6dc77ea9a2c2f2f02d
Arvin Radan <76413127+ArvinRad@users.noreply.github.com>
2021-04-06 21:16:51 -0700

    Add files via upload
    Pair-Programming with Lily Habberfield

--- matrix_transposition.js
+++ matrix_transposition.js
@@ -1,45 +1,42 @@
-
 const transpose = function(matrix) {
-  // Put your solution here
-let newMatrix = [];
-for (let i = 0; i < matrix[0].length; i++) {
-  newMatrix.push([]);
-}
-for (let r = 0; r < matrix.length; r++) {
-  for (let c = 0; c < matrix[r].length; c++) {
-   newMatrix[c].push(matrix[r][c]);
+  const newMatrix = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix[i] = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newMatrix[i].push(matrix[j][i]);
+    }
   }
-}
-return newMatrix;
-}
+  return newMatrix;
+};
 
 // Do not edit this function.
 const printMatrix = (matrix) => {
   for (const row of matrix) {
-      for (const el of row) {
-          process.stdout.write(el + " ");
-      }
-      process.stdout.write('\n')
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write('\n');
   }
-}
+};
 
 printMatrix(transpose([
-[1, 2, 3, 4],
-[1, 2, 3, 4],
-[1, 2, 3, 4],
-[1, 2, 3, 4]
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4]
 ]));
 
-console.log('----')
+console.log('----');
 
 printMatrix(transpose([
-[1, 2],
-[3, 4],
-[5, 6]
+  [1, 2],
+  [3, 4],
+  [5, 6]
 ]));
 
-console.log('----')
+console.log('----');
 
 printMatrix(transpose([
-[1, 2, 3, 4, 5, 6, 7]
+  [1, 2, 3, 4, 5, 6, 7]
 ]));
